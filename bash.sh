bf7b0a2a7db4159a5ad0659034f4205dc703f05e
skulldaime <caiorocha08@hotmail.com>
2021-10-07 07:56:57 -0300

    bash updates

--- bash.sh
+++ bash.sh
@@ -1,4 +1,15 @@
 npm install express
-npm install request
 npm install body-parser
-npm install ejs
+npm install jsx
+npm install graphql-request
+npm install bootstrap
+npm install react
+npm install react-dom
+npm install react-bootstrap
+cd server
+npm install express-react-views
+npm install sequelize
+npm install apollo-server
+npm install dotenv
+npm install graphql
+npm install pg
